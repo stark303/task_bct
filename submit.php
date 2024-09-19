329d248d1bd072c554925de8f603f06586cc16f7
Your Name <you@example.com>
2024-09-19 17:35:36 +0000

    1st

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "database-1.xxxx.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "Bitcot1122";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$sql = "INSERT INTO users (name) VALUES ('$name')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
